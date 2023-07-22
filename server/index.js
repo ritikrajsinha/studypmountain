858bcfb6daf7b31ee5e77d782f90733e4c76f233
ritikrajsinha <85019447+ritikrajsinha@users.noreply.github.com>
2023-07-23 01:53:12 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -12,7 +12,6 @@ const cors = require("cors");
 const {cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
-require("dotenv").config();
 dotenv.config();
 const PORT = process.env.PORT || 4000;
 
