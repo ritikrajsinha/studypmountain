e6cbd9ef93f0938171b33975f842f87a73b95e94
Ritik raj <sinharitikraj@gmail.com>
2023-07-22 16:17:35 +0530

    studynotion

--- server/index.js
+++ server/index.js
@@ -3,8 +3,8 @@ const app = express();
 
 const userRoutes = require("./routes/User");
 const profileRoutes = require("./routes/Profile");
-const paymentRoutes = require("./routes/Payments");
-const courseRoutes = require("./routes/Course");
+
+
 const contactUsRoute = require("./routes/Contact");
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
@@ -40,8 +40,6 @@ cloudinaryConnect();
 //routes
 app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
-app.use("/api/v1/course", courseRoutes);
-app.use("/api/v1/payment", paymentRoutes);
 app.use("/api/v1/reach", contactUsRoute);
 
 //def route
